8f4de60e18667b8fe7073e2c00d493b79534264a
Gabriel Rodrigues <gabrielrodrigues.dev409@gmail.com>
2022-06-26 14:43:43 -0300

    Aula 5 ✅

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,32 +1,9 @@
-import { gql, useQuery } from '@apollo/client'
 import { Lesson } from './Lesson'
 
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
-      id
-      lessonType
-      availableAt
-      title
-      slug
-    }
-  }
-`
-
-interface GetLessonsQueryResponse {
-  lessons: Array<{
-    id: string
-    title: string
-    slug: string
-    availableAt: string
-    lessonType: 'live' | 'class'
-  }>
-}
+import { useGetLessonsQuery } from '../graphql/generated'
 
 export function Sidebar() {
-  const { data } = useQuery<GetLessonsQueryResponse>(GET_LESSONS_QUERY)
-
-  console.log(data?.lessons)
+  const { data } = useGetLessonsQuery()
 
   return (
     <aside className="w-[348px] h bg-gray-700 p-6 border-l border-gray-600">
--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -4,47 +4,20 @@ import { CaretRight, DiscordLogo, FileArrowDown, Lightning, Image, CircleNotch }
 import '@vime/core/themes/default.css'
 import { gql, useQuery } from '@apollo/client'
 import { Link } from 'react-router-dom'
+import { useGetLessonBySlugQuery } from '../graphql/generated'
 
 interface VideoProps {
   lessonSlug: string
 }
 
-interface GetLessonBySlugResponse {
-  lesson: {
-    title: string
-    videoId: string
-    description: string
-    teacher: {
-      name: string
-      bio: string
-      avatarURL: string
-    }
-  }
-}
-
-const GET_LESSON_BY_SLUG_QUERY = gql`
-  query GetLessonBySlug($slug: String) {
-    lesson(where: { slug: $slug }) {
-      title
-      videoId
-      description
-      teacher {
-        name
-        bio
-        avatarURL
-      }
-    }
-  }
-`
-
 export function Video(props: VideoProps) {
-  const { data } = useQuery<GetLessonBySlugResponse>(GET_LESSON_BY_SLUG_QUERY, {
+  const { data } = useGetLessonBySlugQuery({
     variables: {
       slug: props.lessonSlug
     }
   })
 
-  if (!data) {
+  if (!data || !data.lesson) {
     return (
       <div className="flex-1 flex items-center justify-center flex-col">
         <CircleNotch className="animate-spin" size={64} color="#00875F" />
@@ -70,18 +43,20 @@ export function Video(props: VideoProps) {
             <h1 className="text-2xl font-bold">{data.lesson.title}</h1>
             <p className="mt-4 text-gray-200 leading-relaxed">{data.lesson.description}</p>
 
-            <div className="flex items-center gap-4 mt-6">
-              <img
-                className="h-16 w-16 rounded-full border-2 border-blue-500"
-                src={data?.lesson.teacher.avatarURL!}
-                alt=""
-              />
+            {data.lesson?.teacher && (
+              <div className="flex items-center gap-4 mt-6">
+                <img
+                  className="h-16 w-16 rounded-full border-2 border-blue-500"
+                  src={data.lesson.teacher.avatarURL!}
+                  alt=""
+                />
 
-              <div className="leading-relaxed">
-                <strong className="font-bold text-2xl block">{data.lesson.teacher.name}</strong>
-                <span className="text-gray-200 text-sm block">{data.lesson.teacher.bio}</span>
+                <div className="leading-relaxed">
+                  <strong className="font-bold text-2xl block">{data.lesson.teacher.name}</strong>
+                  <span className="text-gray-200 text-sm block">{data.lesson.teacher.bio}</span>
+                </div>
               </div>
-            </div>
+            )}
           </div>
 
           <div className="flex flex-col gap-4">

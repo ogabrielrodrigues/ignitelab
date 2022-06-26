891c6f3faf2ee1be3d02c53695b75fcef02aba28
Gabriel Rodrigues <gabrielrodrigues.dev409@gmail.com>
2022-06-26 14:58:10 -0300

    Corrigido bug das imagens

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        blur: 'url(/src/assets/blur.png)'
+        blur: 'url(/puclic/static/blur.png)'
       },
       fontFamily: {
         sans: 'Roboto, sans-serif'
